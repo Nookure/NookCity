2f947449ada50f5fa1741b669d6e8ac8d0c292fb
Hugo Torres <noreply@redactado.es>
2025-04-19 16:04:58 +0200

    style: adjust formatting and indentation in ProxyPlayerWrapper for consistency

--- NookCity-API/src/main/java/com/nookure/nookcity/ProxyPlayerWrapper.java
+++ NookCity-API/src/main/java/com/nookure/nookcity/ProxyPlayerWrapper.java
@@ -2,10 +2,9 @@ package com.nookure.nookcity;
 
 import com.nookure.core.PlayerWrapperBase;
 
-
 public interface ProxyPlayerWrapper extends PlayerWrapperBase {
-  /**
-   * Sends the player to the lobby.
-   */
-  void goToLobby();
+    /**
+     * Sends the player to the lobby.
+     */
+    void goToLobby();
 }
