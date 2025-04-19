c2eb407f8bc0a717ea0a1350c624bc650606731a
Hugo Torres <noreply@redactado.es>
2025-04-19 16:03:37 +0200

    style: adjust formatting and indentation in LobbyCommand for consistency

--- NookCity-Velocity/src/main/java/com/nookure/nookcity/cmd/LobbyCommand.java
+++ NookCity-Velocity/src/main/java/com/nookure/nookcity/cmd/LobbyCommand.java
@@ -4,23 +4,23 @@ import com.nookure.core.CommandSender;
 import com.nookure.core.command.Command;
 import com.nookure.core.command.CommandData;
 import com.nookure.nookcity.VelocityProxyPlayerWrapper;
+import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
 @CommandData(
-    name = "lobby",
-    permission = "nookcity.lobby",
-    description = "Teleport to the lobby.",
-    aliases = {"hub"}
-)
+        name = "lobby",
+        permission = "nookcity.lobby",
+        description = "Teleport to the lobby.",
+        aliases = {"hub"})
 public class LobbyCommand extends Command {
-  @Override
-  public void onCommand(@NotNull CommandSender commandSender, @NotNull String s, @NotNull List<String> list) {
-    if (commandSender instanceof VelocityProxyPlayerWrapper wrapper) {
-      wrapper.goToLobby();
-    } else {
-      commandSender.sendMiniMessage("Why you want to go to the lobby? You are a proxy console!");
+    @Override
+    public void onCommand(
+            @NotNull CommandSender commandSender, @NotNull String s, @NotNull List<String> list) {
+        if (commandSender instanceof VelocityProxyPlayerWrapper wrapper) {
+            wrapper.goToLobby();
+        } else {
+            commandSender.sendMiniMessage(
+                    "Why you want to go to the lobby? You are a proxy console!");
+        }
     }
-  }
 }
