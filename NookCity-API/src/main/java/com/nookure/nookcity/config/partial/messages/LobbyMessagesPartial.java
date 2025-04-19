9aad53cfc247442876560125782fbbd4441511fb
Hugo Torres <noreply@redactado.es>
2025-04-19 16:03:45 +0200

    style: adjust indentation in LobbyMessagesPartial for consistency

--- NookCity-API/src/main/java/com/nookure/nookcity/config/partial/messages/LobbyMessagesPartial.java
+++ NookCity-API/src/main/java/com/nookure/nookcity/config/partial/messages/LobbyMessagesPartial.java
@@ -4,10 +4,11 @@ import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
 @ConfigSerializable
 public class LobbyMessagesPartial {
-  public String errorWhileConnecting = """
-      <red><b>◆ ERROR ◆</b>
-      
-      gray>There was an error while connecting to the server.
-      <gray>Please contact an administrator if this issue persists.
-      """;
+    public String errorWhileConnecting =
+            """
+            <red><b>◆ ERROR ◆</b>
+
+            gray>There was an error while connecting to the server.
+            <gray>Please contact an administrator if this issue persists.
+            """;
 }
