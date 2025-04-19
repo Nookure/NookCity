84f416da583fb5bc387959694ff3085563aa26c5
Hugo Torres <noreply@redactado.es>
2025-04-19 16:06:21 +0200

    style: adjust formatting and indentation in VelocityCommandManager for consistency

--- NookCity-Velocity/src/main/java/com/nookure/nookcity/cmd/core/VelocityCommandManager.java
+++ NookCity-Velocity/src/main/java/com/nookure/nookcity/cmd/core/VelocityCommandManager.java
@@ -1,5 +1,7 @@
 package com.nookure.nookcity.cmd.core;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.nookure.core.command.Command;
@@ -10,44 +12,41 @@ import com.velocitypowered.api.command.CommandMeta;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.jetbrains.annotations.NotNull;
 
-import static java.util.Objects.requireNonNull;
-
 public class VelocityCommandManager extends CommandManager {
-  @Inject
-  private ProxyServer proxy;
-  @Inject
-  private NookCity nookCity;
-  @Inject
-  private Injector injector;
+    @Inject private ProxyServer proxy;
+    @Inject private NookCity nookCity;
+    @Inject private Injector injector;
 
-  public void registerCommand(@NotNull Class<? extends Command> command) {
-    requireNonNull(command, "Command class cannot be null!");
-    registerCommand(injector.getInstance(command));
-  }
+    public void registerCommand(@NotNull Class<? extends Command> command) {
+        requireNonNull(command, "Command class cannot be null!");
+        registerCommand(injector.getInstance(command));
+    }
 
-  @Override
-  public void registerCommand(@NotNull Command command) {
-    requireNonNull(command, "Command cannot be null!");
-    com.velocitypowered.api.command.CommandManager commandManager = proxy.getCommandManager();
+    @Override
+    public void registerCommand(@NotNull Command command) {
+        requireNonNull(command, "Command cannot be null!");
+        com.velocitypowered.api.command.CommandManager commandManager = proxy.getCommandManager();
 
-    TemplateCommand templateCommand = new TemplateCommand(command);
-    injector.injectMembers(templateCommand);
+        TemplateCommand templateCommand = new TemplateCommand(command);
+        injector.injectMembers(templateCommand);
 
-    CommandData commandData = command.getCommandData();
+        CommandData commandData = command.getCommandData();
 
-    CommandMeta commandMeta = commandManager.metaBuilder(commandData.name())
-        .aliases(commandData.aliases())
-        .plugin(nookCity)
-        .build();
+        CommandMeta commandMeta =
+                commandManager
+                        .metaBuilder(commandData.name())
+                        .aliases(commandData.aliases())
+                        .plugin(nookCity)
+                        .build();
 
-    commandManager.register(commandMeta, templateCommand);
-  }
+        commandManager.register(commandMeta, templateCommand);
+    }
 
-  @Override
-  public void unregisterCommand(@NotNull Command command) {
-    com.velocitypowered.api.command.CommandManager commandManager = proxy.getCommandManager();
-    CommandData commandData = command.getCommandData();
+    @Override
+    public void unregisterCommand(@NotNull Command command) {
+        com.velocitypowered.api.command.CommandManager commandManager = proxy.getCommandManager();
+        CommandData commandData = command.getCommandData();
 
-    commandManager.unregister(commandData.name());
-  }
+        commandManager.unregister(commandData.name());
+    }
 }
