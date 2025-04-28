191bb3dc8c56e87618e70714e7b727204e2a6552
aivruu <benjaalejandro206@gmail.com>
2025-04-28 00:28:32 -0400

    refactor(ServerStatusTask): use a traditional for-each loop

--- plugin/src/main/java/com/nookure/proxy/utils/task/ServerStatusTask.java
+++ plugin/src/main/java/com/nookure/proxy/utils/task/ServerStatusTask.java
@@ -3,46 +3,43 @@ package com.nookure.proxy.utils.task;
 import com.google.inject.Inject;
 import com.nookure.core.logger.Logger;
 import com.nookure.proxy.utils.manager.ServerStatusManager;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
 
 public class ServerStatusTask implements Runnable {
-    @Inject private ServerStatusManager serverStatusManager;
-    @Inject private Logger logger;
+    @Inject
+    private ServerStatusManager serverStatusManager;
+    @Inject
+    private Logger logger;
 
     @Override
     public void run() {
-        serverStatusManager
-                .getAllServers()
-                .forEach(
-                        server ->
-                                server.ping()
-                                        .whenComplete(
-                                                (ping, throwable) -> {
-                                                    logger.debug(
-                                                            "Server %s was pinged.",
-                                                            server.getServerInfo().getName());
-                                                    logger.debug(
-                                                            "Status: %s",
-                                                            throwable == null
-                                                                    ? "Online"
-                                                                    : "Offline");
+        for (final RegisteredServer server : serverStatusManager.getAllServers()) {
+            server.ping().whenComplete(
+                    (ping, throwable) -> {
+                        logger.debug(
+                                "Server %s was pinged.",
+                                server.getServerInfo().getName());
+                        logger.debug(
+                                "Status: %s",
+                                (throwable == null)
+                                        ? "Online"
+                                        : "Offline");
 
-                                                    if (ping.getPlayers().isPresent()) {
-                                                        logger.debug(
-                                                                "PlayerCount: "
-                                                                        + ping.getPlayers()
-                                                                                .get()
-                                                                                .getOnline());
-                                                        logger.debug(
-                                                                "MaxPlayers: "
-                                                                        + ping.getPlayers()
-                                                                                .get()
-                                                                                .getMax());
-                                                    }
+                        if (ping.getPlayers().isPresent()) {
+                            logger.debug(
+                                    "PlayerCount: {}", ping.getPlayers()
+                                            .get()
+                                            .getOnline());
+                            logger.debug(
+                                    "MaxPlayers: {}", ping.getPlayers()
+                                            .get()
+                                            .getMax());
+                        }
 
-                                                    logger.debug("Version: " + ping.getVersion());
+                        logger.debug("Version: {}", ping.getVersion());
 
-                                                    serverStatusManager.updateServerStatus(
-                                                            server, throwable == null);
-                                                }));
+                        serverStatusManager.updateServerStatus(server, throwable == null);
+                    });
+        }
     }
 }
